5b636e7c11256f6348f1a734d8c1121e14beb0a9
Nathan <nathanhugo5573@gmail.com>
2023-12-31 12:15:37 -0300

    Exercicio com listas

--- ApenasExercercicios/Program.cs
+++ ApenasExercercicios/Program.cs
@@ -1,17 +1,53 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ApenasExercercicios
-{
-    internal class Program
+    
+    namespace ApenasExercicios
     {
-        static void Main(string[] args)
+        internal class Program
         {
+            static void Main(string[] args)
+            {
 
-            Console.ReadLine();
+                Console.Write("How many employees will be registered? ");
+                int n = int.Parse(Console.ReadLine());
+
+                List<Employee> list = new List<Employee>(); // Instanciando a lista
+
+                for (int i = 1; i <= n; i++) //Obtendo informações do funcionário
+                {
+                    Console.WriteLine("Employee #" + i + ":");
+                    Console.Write("Id: ");
+                    int id = int.Parse(Console.ReadLine());
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Salary: ");
+                    double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Employee(id, name, salary)); // adicionando as informações
+                    Console.WriteLine();
+                }
+
+                Console.Write("Enter the employee id that will have salary increase : ");
+                int searchId = int.Parse(Console.ReadLine());
+
+                Employee emp = list.Find(x => x.Id == searchId);
+                if (emp != null)
+                {
+                    Console.Write("Enter the percentage: ");
+                    double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    emp.IncreaseSalary(percentage);
+                }
+                else
+                {
+                    Console.WriteLine("This id does not exist!");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Updated list of employees:");
+                foreach (Employee obj in list)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
         }
     }
-}
